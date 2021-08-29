a6e9847b69fdee68e9bc654315cd7984acac24e8
Shahid Nadaf <6.shahidnadaf@gmail.com>
2021-08-29 13:16:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-// registration form using node,express,mongoDB,HTML,CSS
-// if the code is not executing,first go to backend directory by 'cd backend' and run it
+
 
 const express = require('express');
 const app = express();
-const connect_path = require('./db/connect');     // acquiring mongoose connection from 'connect.js' into app.js
+const connect_path = require('./db/connect');     
 const path = require('path');
 const hbs = require('hbs');
 const port = process.env.PORT || 3000;
@@ -14,12 +13,12 @@ const static_path = path.join(__dirname,"../public");
 const partials_path = path.join(__dirname,"../partials");
 
 
-app.use(express.static(static_path));    // built-in middleware to serve static files
+app.use(express.static(static_path));    
 
 app.set("view engine","hbs");
 hbs.registerPartials(partials_path);
 
-app.use(express.json());                            // this and below line are used to convert data recieved from body into json format.
+app.use(express.json());                            
 app.use(express.urlencoded({extended:false}));
 
 app.get("/",(req,res)=>{
@@ -37,13 +36,13 @@ app.get("/register",(req,res)=>{
 app.post("/register",async (req,res)=>{
     try{
 
-        const pass = req.body.password;                 // We set name="password" in register.hbs for input type="password",this line gets value of it.
-        const cpass = req.body.confirmpassword;         // We set name="confirmpassword" in register.hbs for input type="password",this line gets value of it.
+        const pass = req.body.password;                 
+        const cpass = req.body.confirmpassword;         
 
         if(pass === cpass){
             
             const user_info = new register({
-                firstname : req.body.firstname,             // firstname : is the same name taken from mongodb defined schema
+                firstname : req.body.firstname,             
                 lastname : req.body.lastname,
                 email : req.body.email,
                 gender : req.body.gender,
@@ -53,7 +52,7 @@ app.post("/register",async (req,res)=>{
                 confirmpassword : req.body.confirmpassword
             });
 
-            const saveInfo = await user_info.save();            // we used await because we are using async function
+            const saveInfo = await user_info.save();            
             res.status(200).render("home");
         } 
         else{
@@ -72,7 +71,7 @@ app.post("/login",async (req,res)=>{
 
     try{
 
-        const findEmail = await register.findOne({email:loginEmail});           // 'findOne' returns the document if matched with provided query
+        const findEmail = await register.findOne({email:loginEmail});        
         if(findEmail.password === loginPass){
             res.send("Login successful");
         }
